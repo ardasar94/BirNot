cddb6b18f16899bb6fc0197f349c479d2417be96
ardasar94 <sarihanarda@gmail.com>
2021-04-07 12:48:11 +0300

    Not Api Controller

---
+++ BirNotAPI/DTOs/GetNotDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BirNotAPI.DTOs
+{
+    public class GetNotDto
+    {
+        public int Id { get; set; }
+        public string Baslik { get; set; }
+
+        public string Icerik { get; set; }
+
+        public DateTime SonDegistirilme { get; set; }
+    }
+}
